8620b11f7cc6445f993bcc0f08978014e702b8f9
Fiona Tolliday <fionatolliday@hotmail.com>
2019-06-07 14:12:40 +1000

    setting up Mars tests

--- test/Mars.java
+++ test/Mars.java
@@ -1,2 +1,21 @@
+import org.junit.Before;
+import org.junit.Test;
+
 public class Mars {
+
+    @Before
+    public void setUp() throws Exception {
+        Mars mars = new Mars();
+    }
+
+    @Test
+    public void printsOutEmptyMarsGrid() {
+
+    }
+
+    @Test
+    public void marsHasAnObstacleAtLocation() {
+
+        assert.AssertTrue(mars.printObstacle);
+    }
 }
