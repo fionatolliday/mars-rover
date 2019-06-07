2088f0cb776a9e644f241ecbc05a066236345b1e
Fiona Tolliday <fionatolliday@hotmail.com>
2019-06-07 14:05:06 +1000

    building out Rover test cases

---
+++ src/Rover.java
@@ -0,0 +1,12 @@
+public class Rover {
+
+    public Rover getRover() {
+        return rover;
+    }
+
+    public Rover rover;
+
+    public Rover(){
+        this.rover = rover;
+    }
+}
---
+++ test/RoverTest.java
@@ -0,0 +1,66 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RoverTest {
+
+    @Before
+    public void setUp() throws Exception {
+        Rover rover = new Rover();
+    }
+
+    @Test
+    public void roverIsOnMarsAt00FacingNorth(Position x, Position y, Facing n) {
+        Mars mars = new Mars();
+        assert.AssertTrue(mars.printroverlocation);
+    }
+
+    @Test
+    public void roverCanMoveLeft(Command) {
+
+        assert.AssertTrue(rover.moveleft);
+    }
+
+    @Test
+    public void roverCanMoveRight(Command) {
+
+        assert.AssertTrue(rover.moveright);
+    }
+
+    @Test
+    public void roverCanMoveForward(Command) {
+
+        assert.AssertTrue(rover.moveForward);
+    }
+
+    @Test
+    public void roverCanMoveBackwards(Command) {
+
+        assert.AssertTrue(rover.moveBack);
+    }
+
+    @Test
+    public void roverCanTurnSouth() {
+
+        assert.AssertTrue(rover.turnSouth);
+    }
+
+    @Test
+    public void roverCanTurnEast() {
+
+        assert.AssertTrue(rover.turnEast);
+    }
+
+    @Test
+    public void roverCanTurnWest() {
+
+        assert.AssertTrue(rover.turnWest);
+    }
+
+    @Test
+    public void roverCanTurnNorth() {
+
+        assert.AssertTrue(rover.turnNorth);
+    }
+}
