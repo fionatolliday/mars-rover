fa6b3fab8bde87499f2f54379af97a706ff471ff
Fiona Tolliday <fionatolliday@hotmail.com>
2019-07-02 18:45:30 +1000

    when given an R command, rover must turn right.  Test passing for this method

--- test/RoverTest.java
+++ test/RoverTest.java
@@ -56,7 +56,7 @@ public class RoverTest {
     }
 
     @Test
-    public void whenCommandIsL_RoverTurnsLeft() {
+    public void whenCommandIsL_RoverTurnsLeftChangingFacingDirection() {
         List<List<String>> map = new ArrayList<>();
         List<String> row1 = Arrays.asList(" ", " ", " ");
         map.add(row1);
@@ -71,68 +71,19 @@ public class RoverTest {
         Assert.assertEquals(expectedPosition, actualPosition);
     }
 
-    //    @Test
-//    public void moveRoverForward() {
-//        String expectedForwardPosition = "0,1";
-//        Assert.assertEquals(expectedForwardPosition, rover.moveRoverForward('F'));
-//    }
-//
-//    @Test
-//    public void moveRoverBackward() {
-//        String expectedBackPositionFromNorth11 = "2,1";
-//        Assert.assertEquals(expectedBackPositionFromNorth11, rover.moveRoverBackward('B'));
-
-//    }
-
-//    @Test
-//    public void roverCanTurnLeftAndChangeFacingDirection() {
-//        char expectedWest = 'W';
-//        Assert.assertEquals(expectedWest, rover.changeFacingDirectionOfRover('L'));
-//
-//        char expectedSouth = 'S';
-//        Assert.assertEquals(expectedSouth, rover.changeFacingDirectionOfRover('L'));
-//
-//        char expectedEast = 'E';
-//        Assert.assertEquals(expectedEast, rover.changeFacingDirectionOfRover('L'));
-//
-//        char expectedNorth = 'N';
-//        Assert.assertEquals(expectedNorth, rover.changeFacingDirectionOfRover('L'));
-//    }
-
-
-//    @Test
-//    public void roverCanTurnRight() {
-//        char expectedEast = 'E';
-//        Assert.assertEquals(expectedEast, rover.changeFacingDirectionOfRover('R'));
-//
-//        char expectedSouth = 'S';
-//        Assert.assertEquals(expectedSouth, rover.changeFacingDirectionOfRover('R'));
-//
-//        char expectedWest = 'W';
-//        Assert.assertEquals(expectedWest, rover.changeFacingDirectionOfRover('R'));
-//
-//        char expectedNorth = 'N';
-//        Assert.assertEquals(expectedNorth, rover.changeFacingDirectionOfRover('R'));
-//
-//    }
-
-//    @Test
-//    public void marsGridWrapsAtEdge() {
-//        int expectedPosition = 2;
-//        int actual = rover.checkForEdge(-1);
-//
-//        Assert.assertEquals(expectedPosition, actual);
-//    }
-
     @Test
-    public void commandsMoveRover() {
-        ArrayList<Character> userCommands = new ArrayList<>();
-        userCommands.add('F');
-        userCommands.add('F');
-        userCommands.add('L');
-        userCommands.add('B');
-        rover.moveRover(userCommands);
+    public void whenCommandIsR_RoverTurnsRightChangingFacingDirection() {
+        List<List<String>> map = new ArrayList<>();
+        List<String> row1 = Arrays.asList(" ", " ", " ");
+        map.add(row1);
+
+        List<Character> commands = Arrays.asList('R');
+
+        rover.landRover(map, 0, 0, 'N');
+        rover.moveRover(commands);
+        String actualPosition = rover.getPosition();
+        String expectedPosition = "0,0,E";
+
+        Assert.assertEquals(expectedPosition, actualPosition);
     }
-
-
 }
