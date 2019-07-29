161acd1281c141dd30d5f370e0de65b36299d738
Fiona Tolliday <fionatolliday@hotmail.com>
2019-07-29 14:19:41 +1000

    refactored RoverDirectionChanger to include only one method.  Refactored Direction enums to adjust for left and right

--- src/enums/Direction.java
+++ src/enums/Direction.java
@@ -2,5 +2,49 @@ package enums;
 
 public enum Direction {
 
-   NORTH, SOUTH, EAST, WEST
+   NORTH {
+      public Direction getLeft(){
+         return WEST;
+      }
+
+      public Direction getRight(){
+         return EAST;
+      }
+   },
+
+
+   SOUTH {
+      public Direction getLeft(){
+         return EAST;
+      }
+
+      public Direction getRight(){
+         return WEST;
+      }
+   },
+
+
+   EAST {
+      public Direction getLeft(){
+         return NORTH;
+      }
+
+      public Direction getRight(){
+         return SOUTH;
+      }
+   },
+
+
+   WEST {
+      public Direction getLeft(){
+         return SOUTH;
+      }
+
+      public Direction getRight(){
+         return NORTH;
+      }
+   };
+
+   public abstract Direction getLeft();
+   public abstract Direction getRight();
 }
--- src/rover/RoverDirectionChanger.java
+++ src/rover/RoverDirectionChanger.java
@@ -1,6 +1,5 @@
 package rover;
 import enums.Command;
-import enums.Direction;
 
 public class RoverDirectionChanger {
 
@@ -9,62 +8,17 @@ public class RoverDirectionChanger {
     {
         switch (turningDirection){
             case LEFT:
-                return changeRoverFacingDirectionToLeft(currentRoverPosition);
+                return new RoverPosition(currentRoverPosition.getPositionX(),
+                        currentRoverPosition.getPositionY(),
+                        currentRoverPosition.getFacingDirection().getLeft());
 
             case RIGHT:
-                return changeRoverFacingDirectionToRight(currentRoverPosition);
+                return new RoverPosition(currentRoverPosition.getPositionX(),
+                        currentRoverPosition.getPositionY(),
+                        currentRoverPosition.getFacingDirection().getRight());
 
             default:
                 throw new IllegalArgumentException("Unrecognized direction");
-        }
-    }
-
-    private RoverPosition changeRoverFacingDirectionToLeft(RoverPosition currentRoverPosition) {
-
-        switch (currentRoverPosition.getFacingDirection()) {
-            case NORTH:
-                return new RoverPosition(currentRoverPosition.getPositionX(),
-                        currentRoverPosition.getPositionY(), Direction.WEST);
-
-            case EAST:
-                return new RoverPosition(currentRoverPosition.getPositionX(),
-                        currentRoverPosition.getPositionY(), Direction.NORTH);
-
-            case SOUTH:
-                return new RoverPosition(currentRoverPosition.getPositionX(),
-                        currentRoverPosition.getPositionY(), Direction.EAST);
-
-            case WEST:
-                return new RoverPosition(currentRoverPosition.getPositionX(),
-                        currentRoverPosition.getPositionY(), Direction.SOUTH);
-
-            default:
-                throw new IllegalArgumentException("Invalid facing direction");
-
-        }
-    }
-
-    private RoverPosition changeRoverFacingDirectionToRight(RoverPosition currentRoverPosition) {
-
-        switch (currentRoverPosition.getFacingDirection()) {
-            case NORTH:
-                return new RoverPosition(currentRoverPosition.getPositionX(),
-                        currentRoverPosition.getPositionY(), Direction.EAST);
-
-            case EAST:
-                return new RoverPosition(currentRoverPosition.getPositionX(),
-                        currentRoverPosition.getPositionY(), Direction.SOUTH);
-
-            case SOUTH:
-                return new RoverPosition(currentRoverPosition.getPositionX(),
-                        currentRoverPosition.getPositionY(), Direction.WEST);
-
-            case WEST:
-                return new RoverPosition(currentRoverPosition.getPositionX(),
-                        currentRoverPosition.getPositionY(), Direction.NORTH);
-
-            default:
-                throw new IllegalArgumentException("Invalid facing direction");
         }
     }
 
